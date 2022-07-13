e8a69072545be1551509f3c1b990a8a27f35dccd
dhwldwld <brown8167@gmail.com>
2022-07-13 17:22:24 +0900

    feat: redux-thunk

--- App.js
+++ App.js
@@ -22,11 +22,12 @@ import React from 'react';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import CounterListContainer from './src/containers/CounterListContainer';
 
-import { createStore } from 'redux';
-import reducers from './src/reducers';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './src/reducers';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const App = () => {
     return (
